a89bbbf8abbfda2b32c799999b8d8375b877c6b1
Paulo Santos <paulo.santos@buser.com.br>
2023-07-02 00:34:12 -0300

    - Creates custom ProfileManager and Profile models - Defines Profile as the AUTH_USER_MODEL on settings - Creates user_api's first migration

--- users_api/models.py
+++ users_api/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class ProfileManager(BaseUserManager):
+	def create_user(self, email, password=None, username=None, name=None):
+		if not email or not password:
+			raise ValueError('Both email and password are needed')
+		email = self.normalize_email(email)
+		user = self.model(email=email, username=username, name=name)
+		user.set_password(password)
+		user.save()
+		return user
+
+	def create_superuser(self, email, password=None, username=None, name=None):
+		if not email or not password:
+			raise ValueError('Both email and password are needed')
+		user = self.create_user(email, password, username, name)
+		user.is_superuser = True
+		user.is_staff = True
+		user.save()
+		return user
+
+
+class Profile(AbstractBaseUser, PermissionsMixin):
+	profile_id = models.AutoField(primary_key=True)
+	email = models.EmailField(max_length=50, unique=True)
+	username = models.CharField(max_length=50, unique=True)
+	name = models.CharField(max_length=50)
+	is_superuser = models.BooleanField(default=False)
+	is_staff = models.BooleanField(default=False)
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username', 'name']
+
+	objects = ProfileManager()
+
+	def __str__(self):
+		return self.username
+
