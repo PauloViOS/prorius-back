c03eb4c821de56c4a203ca911394f4145cacea6e
Paulo Santos <paulo.santos@buser.com.br>
2023-07-04 10:06:28 -0300

    - Changes list_display order on ProfileAdmin

--- users_api/admin.py
+++ users_api/admin.py
@@ -5,4 +5,4 @@ from users_api.models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-	list_display = ['profile_id', 'email', 'username', 'name', 'is_deleted', 'is_staff', 'is_superuser']
+	list_display = ['profile_id', 'email', 'username', 'name', 'is_staff', 'is_superuser', 'is_deleted']
